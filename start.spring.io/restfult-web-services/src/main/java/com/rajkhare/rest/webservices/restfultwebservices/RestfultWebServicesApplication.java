2c858c2107bd2c1e7d6bc1ced22f7b3f6a0809ba
rajkhare1 <raj.khare.java@gmail.com>
2024-02-28 05:04:13 +0530

    28-02-2024:update

---
+++ start.spring.io/restfult-web-services/src/main/java/com/rajkhare/rest/webservices/restfultwebservices/RestfultWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.rajkhare.rest.webservices.restfultwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfultWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfultWebServicesApplication.class, args);
+	}
+
+}
---
+++ start.spring.io/restfult-web-services/src/test/java/com/rajkhare/rest/webservices/restfultwebservices/RestfultWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajkhare.rest.webservices.restfultwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfultWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
